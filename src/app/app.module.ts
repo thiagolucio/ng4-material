7abb8e3135e00ab6c6da69f8fed90fc70c625346
thiagolucio <thiagolucio@gmail.com>
2017-11-29 10:14:35 -0200

    adicionando novas informacoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
